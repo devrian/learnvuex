25c429725fa3a5d43b04800f0e150f328f830571
devriansyah <devriansyah@docotel.com>
2018-06-21 15:07:23 +0700

    make component member

---
+++ src/components/Member.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="members">
+        <h3>{{ member.name }} - {{ member.point }}</h3>
+        <img :src="'./static/images/' + member.img" width="200">
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'member'
+        ]   
+    }
+</script>
+
